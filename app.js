431d6876a04624cf56112a5b428970d918cded5a
Oscar Icochea Calenzani <o.icochea09@gmail.com>
2020-08-24 02:20:30 -0400

    added phone call capabilities,added pinned , nav, added backdrop, working on drag and drop , and did cosmetic touches

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ console.log("linked")
 // var passName =""
 
 
+
+
 $('#beerButton').on('click', (event) => {
     const zipCode = $('input').val();  
     console.log(zipCode)
     event.preventDefault();
+
+
 
 // const promise = $.ajax({
 //     url:`https://api.openbrewerydb.org/breweries?by_postal=${zipCode}`
@@ -13,43 +17,30 @@ $('#beerButton').on('click', (event) => {
 
 
 $.ajax({
-    url: `https://api.openbrewerydb.org/breweries?by_postal=${zipCode}`,
+    url: `https://api.openbrewerydb.org/breweries?by_postal=${zipCode}`
+    ,
+    
   }).then(
       (data) => {
         $('#brewery').children().remove();
            let  i= (Math.floor(Math.random() * [data.length])) 
+        // if(data[i].name == " "){
         //    passName = data[i].name
-        $('#brewery').append($(`<li> ${data[i].name} </li> </br>`))
+        $('#brewery').append($(`<li> ${data[i].name} </li>`).attr("draggable","true").attr("id" , "currentBrewery"))
         $('li').append($(`<p> Street Address: ${data[i].street} , ${data[i].city} , ${data[i].state}</p>`));
-    } 
+
+        //    }else{
+          
+        //    }
+      })  
+               
 , ()=>{
         console.log("bad request")
-    });
+       
 
-
-
-    // const promise2 = $.ajax({
-    //     url:`https://serpapi.com/playground?q=${passName}&tbm=isch&ijn=0`
-    // });
-    
-    
-    // $.ajax({
-    //     url: `https://serpapi.com/playground?q=${passName}&tbm=isch&ijn=0`,
-    //   }).then(
-    //       (data) => {
-    //         var imageUrl = data.images_results[0].link
-    //         $('li').css("background-image", "url(" + imageUrl + ")")
-    //     } 
-    // , ()=>{
-    //         console.log("bad request")
-    //     });
-
+    }
 });
+// });
 
 
 
-$( function() { 
-    $("li").draggable(); 
-} ); 
-
-  
