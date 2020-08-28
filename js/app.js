41931e22b1e9f9549b352a83d4ea3ddea4aef406
Oscar Icochea Calenzani <o.icochea09@gmail.com>
2020-08-28 18:10:23 -0400

    images

--- js/app.js
+++ js/app.js
@@ -67,7 +67,7 @@ $.ajax({
   
 }).then(
     (data) => {
-       $('.brewery1').css("background-image", "url(../dubc.jpg)")
+       $('.brewery1').css("background-image", "url(/Users/oscaricochea/Desktop/seir-flex-zen-work/projects/Project_1/dubc.jpg)")
       $('.brewery1').append($(`<li> ${data[0].name} </li>`).attr("id" , "creatorsFavs").addClass("editPick1"))
       $('.editPick1').append($(`<p> Street Address: ${data[0].street}       , ${data[0].city} , ${data[0].state}</p>`))
       $('.link1').attr("href", `${data[0].website_url}`)
@@ -84,7 +84,7 @@ $.ajax({
         }).then(
             (data) => {
 
-               $('.brewery2').css("background-image", "url(gsb.png)")
+               $('.brewery2').css("background-image", "url(/Users/oscaricochea/Desktop/seir-flex-zen-work/projects/Project_1/gsb.png)");
               $('.brewery2').append($(`<li> ${data[2].name} </li>`).attr("id" , "creatorsFavs").addClass("editPick2"))
               $('.editPick2').append($(`<p> Street Address: ${data[2].street}       , ${data[2].city} , ${data[2].state}</p>`))
               $('.link2').attr("href", `${data[2].website_url}`)
@@ -104,7 +104,7 @@ $.ajax({
             }).then(
                 (data) => {
     
-                   $('.brewery3').css("background-image", "url(../lirb.jpeg)")
+                   $('.brewery3').css("background-image", "url(/Users/oscaricochea/Desktop/seir-flex-zen-work/projects/Project_1/lirb.jpeg)")
                   $('.brewery3').append($(`<li> ${data[4].name} </li>`).attr("id" , "creatorsFavs").addClass("editPick3"))
                   $('.editPick3').append($(`<p> Street Address: ${data[4].street}       , ${data[4].city} , ${data[4].state}</p>`))
                   $(',phone3').attr ("href" , `tel:${data[4].phone}`)   
@@ -121,7 +121,7 @@ $.ajax({
                 }).then(
                     (data) => {
         
-                       $('.brewery4').css("background-image", "url(../obbc.jpg")
+                       $('.brewery4').css("background-image", "url(/Users/oscaricochea/Desktop/seir-flex-zen-work/projects/Project_1/obbc.jpg)")
                       $('.brewery4').append($(`<li> ${data[0].name} </li>`).attr("id" , "creatorsFavs").addClass("editPick4"))
                       $('.editPick4').append($(`<p> Street Address: ${data[0].street}       , ${data[0].city} , ${data[0].state}</p>`))
                       $('.link4').attr("href", `${data[0].website_url}`) 
